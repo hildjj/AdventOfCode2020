3df9140e31a72c20c9d418aadbf9eb85d386ea8e
Joe Hildebrand <joe-github@cursive.net>
2020-12-03 11:50:12 -0700

    save several seconds of typing each day by defaulting .txt file name

--- utils.js
+++ utils.js
@@ -1,12 +1,30 @@
 'use strict'
 const fs = require('fs')
+const path = require('path')
+
+function getStack(_, stack) {
+  return stack
+}
 
 class Utils {
   static readLines(filename) {
+    if (!filename) {
+      // this would be idiomatic in tcl.
+      // s/.js$/.txt/ from the calling file.
+      filename = path.parse(this.callsites()[1].getFileName()).name + '.txt'
+    }
     return fs.readFileSync(filename, 'utf8')
       .split('\n')
       .filter(s => s.length)
   }
+  static callsites() {
+    const old = Error.prepareStackTrace
+	  Error.prepareStackTrace = getStack
+	  const stack = new Error().stack.slice(1) // i am never interesting
+	  Error.prepareStackTrace = old
+	  return stack
+  }
 }
 
 module.exports = Utils
+
